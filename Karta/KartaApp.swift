e4649c4f07a31ba7635adda8fef96661c7ad8b79
Mirdinic <mirdin.sadykov@mail.ru>
2021-08-11 17:37:33 +0600

    Initial Commit

---
+++ Karta/KartaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KartaApp.swift
+//  Karta
+//
+//  Created by Mirdin Sadykov on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct KartaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
