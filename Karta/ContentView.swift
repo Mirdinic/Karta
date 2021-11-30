faca295173c3b8fbe79c54db74cbb9bcbca9bf9e
Mirdinic <mirdins@bk.ru>
2021-11-30 19:05:53 +0600

    initial commit

--- Karta/ContentView.swift
+++ Karta/ContentView.swift
@@ -8,9 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card5"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack(){
+            Image("background")
+            VStack() {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack(){
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    if playerRand > cpuRand{
+                        playerScore += 1
+                    }
+                    else if playerRand < cpuRand {
+                        cpuScore += 1
+                    }
+                }, label: {
+                    
+                    Image("dealbutton")
+                })
+                Spacer()
+                Spacer()
+                HStack(){
+                    Spacer()
+                    VStack(){
+                        Text("Player")
+                            .font(.title2)
+                        .foregroundColor(Color.white)
+                        
+                        Text(String(playerScore))
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .padding(.vertical, 20.0)
+                        
+                    }
+                    Spacer()
+                    VStack(){
+                        Text("CPU")
+                            .font(.title)
+                        .foregroundColor(Color.white)
+                        Text(String(cpuScore))
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .padding(.vertical)
+                    }
+                    Spacer()
+                
+                    
+                }
+                Spacer()
+                
+            }
+        }
     }
 }
 
